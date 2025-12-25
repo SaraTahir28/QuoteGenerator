80580ca144a9b5d1e2732c5cb4557c33bd6a3bed
Sara Tahir <saratahir34@gmail.com>
2025-12-25 12:51:28 +0000

    Updating Urls for the app to work in production

--- FrontEnd/quotes.js
+++ FrontEnd/quotes.js
@@ -8,7 +8,7 @@ function pickFromArray(choices) {
  //converting this to a function that fetches from backend 
 async function displayRandomquote(){
   try{
-  const response = await fetch("http://127.0.0.1:3000/") //talking to backend with a GET request, which only sends back data.
+  const response = await fetch("https://mscgsco4cg4wkok4g4go0s0c.hosting.codeyourfuture.io/") //talking to backend with a GET request, which only sends back data.
   const data = await response.json() //expecting data to be sent as structured JSON. 
 
   document.getElementById("quote").textContent = data.quote;
@@ -29,7 +29,7 @@ document.getElementById("add-quote-form").addEventListener("submit",async(e)=>{
   const quote = document.getElementById("new-quote-text").value;
   const author = document.getElementById("new-quote-author").value;
   try { 
-    const response = await fetch("http://127.0.0.1:3000/", {
+    const response = await fetch("https://mscgsco4cg4wkok4g4go0s0c.hosting.codeyourfuture.io/", {
         method: "POST", 
         headers: { "Content-Type": "application/json" }, 
         body: JSON.stringify({ quote, author }) }); 
